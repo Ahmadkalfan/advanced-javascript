d1c4f4c2616aa9a2558870ef8e47815e1b48d7a6
Ahmadkalfan <kalfan697@gmail.com>
2020-12-29 18:52:51 +0800

    scope-finished

---
+++ scope.js
@@ -0,0 +1,29 @@
+let bonus=20;
+function sum(first,second){
+    const result=first+second+bonus;
+    if (result>10) {
+        var mood='happy'
+         mood='funky'
+         mood='cracky'
+         mood='balty'
+         console.log(mood);
+    }
+    console.log(mood);
+    // console.log(result);
+    // console.log(bonus);
+    return result;
+
+
+   
+   
+}
+let day='friday';
+console.log(day);
+
+
+
+
+const output=sum(10,10);
+// console.log(bonus);
+
+//  console.log(output);
