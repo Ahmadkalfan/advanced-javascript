ebcecc0934a5c21ba45e4f8734710e3acc23d35a
Ahmadkalfan <kalfan697@gmail.com>
2020-12-29 10:54:15 +0800

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,37 @@
+const name=undefined;
+console.log(name);
+
+let pakhi;
+console.log(pakhi);
+
+function sum(num1,num2){
+      console.log(num1+num2);
+   
+}
+let total=sum(12,34)
+console.log(total);
+
+function total1(num1,num2){
+    console.log(num1+num2);
+     return
+}
+let sumTotal=total1(5,3)
+console.log(sumTotal);
+
+function total2(num1,num2){
+    console.log(num1+num2);
+     return
+}
+let sumTotal1=total2(5)
+console.log(sumTotal1);
+
+
+let names={
+    name:'kalfan',id:15151
+}
+console.log(names.gfName);
+
+
+
+
+
