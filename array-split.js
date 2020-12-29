e0743a55f4a562c51de4dfa898a425cea54c98e0
Ahmadkalfan <kalfan697@gmail.com>
2020-12-29 19:54:14 +0800

    array-split

---
+++ array-split.js
@@ -0,0 +1,9 @@
+const nums=[1,2,3,4,5,6,7,8,9];
+// const part=nums.slice(2,5);
+ const removed=nums.splice(2,5,23,23,23);  //you can remove the element and replace it with another element by using splice
+// console.log(part);
+//  console.log(removed);
+// console.log(nums);
+
+const together=nums.join('ami');
+console.log(together);
